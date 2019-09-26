353dc52c1b263cc8f2c68ef9a052904bf13b3b75
khoadoan0401 <doankhoa.dtak@gmail.com>
2019-09-26 15:19:33 +0700

    ex04

--- ex04/ex04/Program.cs
+++ ex04/ex04/Program.cs
@@ -6,11 +6,32 @@ using System.Threading.Tasks;
 
 namespace ex04
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
 
+        }
+        public static bool IsOdd(int n)
+        {
+           if (n %2 != 0)
+           {
+               return true;
+           }
+               return false;
+        }
+        public static bool IsPrime(int n)
+        {
+            if (n <= 1)
+            {
+                return false;
+            }
+            for (int i = 2; i < n; i++)
+            {
+                if (n % 1 == 0)
+                    return false;
+            }
+                    return true;
         }
     }
 }
